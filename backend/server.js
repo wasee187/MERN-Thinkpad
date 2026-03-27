243fb3e8c34cc2f1e65407f9234f845c0fbd3aec
wasee187 <waseesarwar187@gmail.com>
2026-03-27 08:33:40 +0200

    Basic backend server setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import express from "express";
 
 const app = express();
+
+app.get("/api/notes", (req,res)=>{
+    console.log("You have 5 notes");
+}); 
 app.listen(5001, ()=>{
     console.log("Server is running on PORT: 5001")
 }); 
