6cbe12acdc9214a3b2c71235d29156495968bb3d
wasee187 <waseesarwar187@gmail.com>
2026-03-27 09:11:54 +0200

    npm nodemon package added and post, put, delete route added

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,18 @@ import express from "express";
 const app = express();
 
 app.get("/api/notes", (req,res)=>{
-    console.log("You have 5 notes");
+    res.status(200).send("You have 10 notes")
 }); 
+app.post("/api/notes", (req,res)=>{
+    res.status(201).json({ message: "Note created successfully"})
+});
+app.put("/api/notes/:id", (req,res)=>{
+    res.status(200).json({ message: "Note updated successfully"})
+});
+app.delete("/api/notes/:id", (req,res)=>{
+    res.status(201).json({ message: "Note deleted successfully"})
+});
+
 app.listen(5001, ()=>{
     console.log("Server is running on PORT: 5001")
 }); 
